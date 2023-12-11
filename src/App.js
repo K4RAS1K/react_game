811f26f66070d3f22cee5b3222cad6fae65ff809
Lobanov Nikita <lobanovns01@gmail.com>
2023-12-11 11:42:57 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Monster from "./components/UI/monster/Monster";
 
 function App() {
+  const [hero,setHero] = useState([
+    {streangh:1,body:'Description'},
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
+      <Monster/>
     </div>
   );
 }
